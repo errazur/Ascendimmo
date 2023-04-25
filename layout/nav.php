2a5c754e0ce367082e3bdafc3a3dd9d20f70fb73
errazur <systemhorscombat@gmail.com>
2023-04-25 12:49:29 +0200

    update nav

--- layout/nav.php
+++ layout/nav.php
@@ -1,10 +1,10 @@
 <nav class="navbar navbar-light navbar-expand-md fixed-top" id="mainNav">
-    <div class="container"><a class="navbar-brand" href="#">Brand</a><button data-bs-toggle="collapse" class="navbar-toggler navbar-toggler-right" data-bs-target="#navbarResponsive" type="button" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation" value="Menu"><i class="fa fa-bars"></i></button>
+    <div class="container"><a class="navbar-brand" href="/">Ascendimmo</a><button data-bs-toggle="collapse" class="navbar-toggler navbar-toggler-right" data-bs-target="#navbarResponsive" type="button" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation" value="Menu"><i class="fa fa-bars"></i></button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ms-auto">
-                <li class="nav-item nav-link"><a class="nav-link active" href="#about">About</a></li>
+                <li class="nav-item nav-link"><a class="nav-link active" href="/login">About</a></li>
                 <li class="nav-item nav-link"></li>
-                <li class="nav-item nav-link"><a class="nav-link" href="#contact">contact</a></li>
+                <li class="nav-item nav-link"><a class="nav-link" href="/signin">contact</a></li>
             </ul>
         </div>
     </div>
